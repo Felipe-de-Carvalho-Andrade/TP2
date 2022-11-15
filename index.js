1e455233c1d3f609fb19c1edab0daff25714779b
Felipe-de-Carvalho-Andrade <felipea07112006@gmail.com>
2022-11-15 13:16:39 -0300

    05

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
