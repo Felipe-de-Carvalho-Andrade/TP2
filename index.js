74e9105afcc40f3c455791fb9dc95695c74178e7
Felipe-de-Carvalho-Andrade <felipea07112006@gmail.com>
2022-11-15 17:18:31 -0300

    07

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -10,3 +11,35 @@ let swiper = new Swiper(".home", {
       clickable: true,
     },
 });
+=======
+let swiper = new Swiper("#fundo", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//Show Video
+let playButton = document.querySelectorAll(".bx-play");
+let video = document.querySelectorAll(".video-container");
+let trailer = document.querySelectorAll(".trailer");
+let close = document.querySelectorAll(".close-video");
+
+for (let i = 0; i < playButton.length; i++) {
+  playButton[i].onclick = () => {
+    video[i].classList.add("show-video");
+    trailer[i].play();
+  };
+  close[i].onclick = () => {
+    video[i].classList.remove("show-video");
+    trailer[i].pause();
+  }
+};
+
+>>>>>>> Stashed changes
