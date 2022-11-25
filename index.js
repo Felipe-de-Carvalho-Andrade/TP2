78588c5962683c15baff240fd7495a5abb6d6e3f
brivyy <vieirabruno28112005@gmail.com>
2022-11-25 15:21:28 -0300

    login2

--- index.js
+++ index.js
@@ -10,3 +10,51 @@ let swiper = new Swiper(".home", {
     clickable: true,
   },
 });
+
+let usuario = { nome: "", senha: "" };
+let $nome = $("#nome");
+let $senha = $("#senha");
+let lsEL = localStorage.getItem("usuario");
+
+if (lsEL != undefined) {
+  usuario = JSON.parse(lsEL);
+  $nome.val(usuario.nome);
+  $senha.val(usuario.senha);
+}
+
+$("#form-login").submit((e) => {
+  e.preventDefault();
+  $("#telalogin").toggle();
+
+usuario.nome = $nome.val();
+usuario.senha = $senha.val();
+let $checkbox = $("#salvarcredenciais").prop('checked');
+
+  sessionStorage.setItem("usuario", JSON.stringify(usuario));
+
+  if ($checkbox) {
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+  }
+  else {
+    let apagar = localStorage.getItem("usuario");
+
+    if (apagar != undefined) {
+      localStorage.removeItem("usuario");
+    }
+  }
+
+  $nome.val("");
+  $senha.val("");
+});
+
+$('#botaologin').on('click', ()=>{
+  
+  $("#telalogin").toggle();
+
+})
+
+$('#x').on('click', ()=>{
+  
+  $("#telalogin").toggle();
+
+})
