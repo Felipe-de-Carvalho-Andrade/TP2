c725c301a934f0f6295249427563770035a7cc0c
Bruno Vieira <106450850+brivyy@users.noreply.github.com>
2022-11-30 16:10:23 +0000

    termino

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function checklogin(user){
+function checklogin(user) {
   user.forEach((nomes) => {
     if (nomes.nome == $("#nome").val() && nomes.senha == $("#senha").val()) {
       usuario.senha = nomes.senha;
@@ -6,7 +6,8 @@ function checklogin(user){
       sessionStorage.setItem("usuario", JSON.stringify(usuario));
       localStorage.setItem("usuario", JSON.stringify(usuario));
       return 0;
-  }});
+    }
+  });
   return 1;
 }
 
@@ -25,32 +26,32 @@ let swiper = new Swiper("#fundo", {
 });
 
 //Trailer
-for (let i = 0; i < $('.bx-play').length; i++) {
-  $('.bx-play')[i].onclick = () => {
-    $('.video-container')[i].classList.add("show-video");
-    $('.trailer')[i].play();
+for (let i = 0; i < $(".bx-play").length; i++) {
+  $(".bx-play")[i].onclick = () => {
+    $(".video-container")[i].classList.add("show-video");
+    $(".trailer")[i].play();
   };
-  $('.close-video')[i].onclick = () => {
-    $('.video-container')[i].classList.remove("show-video");
-    $('.trailer').pause();
-  }
-};
+  $(".close-video")[i].onclick = () => {
+    $(".video-container")[i].classList.remove("show-video");
+    $(".trailer").pause();
+  };
+}
 
-//Menu 
-addEventListener('scroll', () => {
-  $('header').toggleClass('menu-cor', window.scrollY > 0);
+//Menu
+addEventListener("scroll", () => {
+  $("header").toggleClass("menu-cor", window.scrollY > 0);
 });
 
-let menu = document.querySelector('#menu-icon');
+let menu = document.querySelector("#menu-icon");
 
 menu.onclick = () => {
-  $('#menu-icon').toggleClass('bx-x');
-  $('.menu').toggleClass('ativo');
-}
+  $("#menu-icon").toggleClass("bx-x");
+  $(".menu").toggleClass("ativo");
+};
 window.onscroll = () => {
-  $('#menu-icon').removeClass('bx-x');
-  $('.menu').removeClass('ativo');
-}
+  $("#menu-icon").removeClass("bx-x");
+  $(".menu").removeClass("ativo");
+};
 
 //Login e Cadastro
 let usuario = {
@@ -148,4 +149,54 @@ $("#botaologin").on("click", () => {
 
 $("#x").on("click", () => {
   $("#telalogin").toggle();
+});
+
+$("#adicionar").click(() => {
+  $("#adicionar-lista").toggle();
+});
+let filmes;
+
+let es = localStorage.getItem("filmes");
+if (es != undefined) {
+  filmes = JSON.parse(es);
+} else {
+  filmes = [
+    {
+      nome: "Kill Bill",
+      descricao: " filme ai",
+      poster:
+        "https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2021/07/01/698803739-kill-bill-uma-thurman.jpg",
+      genero: "Ação",
+    },
+    {
+      nome: "Kill Bifgdfgll",
+      descricao: " filfgdme ai",
+      poster:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FKill_Bill&psig=AOvVaw0seRajd-ubeMDyhkEaQRAk&ust=1669906526797000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCIDC7-GU1vsCFQAAAAAdAAAAABAD",
+      genero: "Ação",
+    },
+  ];
+}
+
+filmes.forEach((se) => {
+  $("#a").append(
+    "<li> <h1>" +
+      se.nome +
+      "<h1> <br> <img src='" +
+      se.poster +
+      "'> <p>" +
+      se.descricao +
+      "</p><p>" +
+      se.genero +
+      "</p></li>"
+  );
+});
+
+$("#novo").click(() => {
+  filmes[filmes.length].nome = $("#titulo").val();
+  filmes[filmes.length].descricao = $("#descricao").val();
+  filmes[filmes.length].poster = $("#poster").val();
+  filmes[filmes.length].genero = $("#sele").val();
+
+  localStorage.setItem("filmes", JSON.stringify(filmes));
 });
